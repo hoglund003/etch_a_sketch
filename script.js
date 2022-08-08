ff6b9e587a13da6a2e1612a5e07a33c5453daaa2
Kasper Høglund <kasper@rubynor.no>
2022-08-08 12:00:57 +0200

    function renderFrame added

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".btn.change");
 
-for (let x = 0; x < 16; x++) {
-    var x_div = document.createElement("div");
-    x_div.setAttribute("class", `x ${x.toString()}`);
-    container.appendChild(x_div);
-
-    for (let y = 0; y < 16; y++) {
-        var y_div = document.createElement("div");
-        y_div.setAttribute("class", `y ${y.toString()}`);
-        x_div.appendChild(y_div);
+function renderFrame(length_x, length_y) {
+    for (let x = 0; x < length_x; x++) {
+        var x_div = document.createElement("div");
+        x_div.setAttribute("class", `x ${x.toString()}`);
+        container.appendChild(x_div);
+    
+        for (let y = 0; y < length_y; y++) {
+            var y_div = document.createElement("div");
+            y_div.setAttribute("class", `y ${y.toString()}`);
+            x_div.appendChild(y_div);
+        }
     }
 }
+
+button.addEventListener("click", function(e) {
+    var length_x = prompt("Length x:");
+    var length_y = prompt("Length y:");
+
+    if (length_x >= 100) {
+        length_x = 100;
+    }
+    if (length_y >= 100) {
+        length_y = 100;
+    }
+    if (length_x < 1) {
+        length_x = 1;
+    }
+    if (length_y < 1) {
+        length_y = 1;
+    }
+
+    renderFrame(length_x, length_y);
+});
+
+renderFrame(80, 80);
